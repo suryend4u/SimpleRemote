751b0ef9bfb6b26ecc927200c483c45f486d85d9
suryendu <surdas13.in@ibm.com>
2017-07-31 00:28:41 +0530

    2nd chg

--- SimpleDemo/src/com/mydemo/example/MyMain.java
+++ SimpleDemo/src/com/mydemo/example/MyMain.java
@@ -5,7 +5,7 @@ public class MyMain {
 	public static void main(String[] args) {
 		System.out.println("***Git works??***");
 		
-		System.out.println("***Git works after first change??***");
+		System.out.println("***Git works after 2nd change??***");
 
 	}
 
